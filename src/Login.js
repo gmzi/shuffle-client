9c2422663b870d23f27c79a5190f510f6c7cdf9d
gmzi <gastonmazieres@gmail.com>
2021-07-07 09:18:51 -0400

    fix redirect url

--- src/Login.js
+++ src/Login.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import logo from './icons/black.png';
 
 const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=b4217743307a432d807c1e5840dde3a2&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative';
+  'https://accounts.spotify.com/authorize?client_id=b4217743307a432d807c1e5840dde3a2&response_type=code&redirect_uri=https://shuffle-client.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private%20playlist-read-collaborative';
 
 export default function Login({ code }) {
   const [recommended, setRecommended] = useState([]);
